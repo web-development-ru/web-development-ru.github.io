1efcf90b913692f7e22bdb11fd42c9a95ce343df
Development <48911160+MRXANANIM@users.noreply.github.com>
2019-06-07 16:36:53 +0600

    site
    site

---
+++ site-TEST/js/main/main.js
@@ -0,0 +1,10 @@
+jQuery(document).ready(function($) {
+	$('.menu-btn').on('click', function(e) {
+		e.preventDefault();
+		$(this).toggleClass('menu-btn_active');
+		$('.menu-nav').toggleClass('menu-nav_active');
+	});
+});
+
+
+
